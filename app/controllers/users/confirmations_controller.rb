30a305ed7c3d787bc46d2f704ac6c0114d4b4500
Phuoc <onhachungphuoc@gmail.com>
2016-07-03 07:49:45 +0700

    push test pronto

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,8 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
   def new
     super
+    "aaaaa"
+    devise_parameter_sanitizer.for(:sign_up) {u.permit %i( email password password_confirmation )}
   end
 
   def create
